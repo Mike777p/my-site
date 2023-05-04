68483c67e963ff7487379cd893c49986cac85647
Mike777p <mjp7@duck.com>
2023-05-04 15:12:07 +0100

    Add click enlargement of certification feature and autoscroll

--- src/Assets/Wrappers/CertificationWrapper.jsx
+++ src/Assets/Wrappers/CertificationWrapper.jsx
@@ -24,6 +24,24 @@ const Wrapper = styled.main`
     text-align: center;
     margin-top: 10px;
   }
+  .modal {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  .modal img {
+    max-width: 80%;
+    max-height: 80%;
+    object-fit: cover;
+    border-radius: 5px;
+  }
   `
 
   export default Wrapper
--- src/Components/Certifications.jsx
+++ src/Components/Certifications.jsx
@@ -1,46 +1,63 @@
-import certificationData from "../Assets/Wrappers/CertificationData"
-import React from 'react';
-import Wrapper from "../Assets/Wrappers/CertificationWrapper"
+import certificationData from "../Assets/Wrappers/CertificationData";
+import React, { useState } from 'react';
+import Wrapper from "../Assets/Wrappers/CertificationWrapper";
 import Slider from 'react-slick';
 
 const Certifications = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
-    ],
+    },
+  ],
+};
+
+
+  const [showModal, setShowModal] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
+
+  const handleImageClick = (image) => {
+    setModalImage(image);
+    setShowModal(true);
   };
 
   return (
     <Wrapper>
-    <div className="certifications">
-      <h2>Certifications</h2>
-      <Slider {...settings}>
-        {certificationData.map((cert, index) => (
-          <div key={index} className="certification">
-            <img src={cert.pdf} alt={cert.title} />
-            <h3>{cert.title}</h3>
-          </div>
-        ))}
-      </Slider>
-    </div>
+      <div className="certifications">
+        <h2>Certifications</h2>
+        <Slider {...settings}>
+          {certificationData.map((cert, index) => (
+            <div key={index} className="certification" onClick={() => handleImageClick(cert.pdf)}>
+              <img src={cert.pdf} alt={cert.title} />
+              <h3>{cert.title}</h3>
+            </div>
+          ))}
+        </Slider>
+      </div>
+      {showModal && (
+        <div className="modal" onClick={() => setShowModal(false)}>
+          <img src={modalImage} alt="modal certification" />
+        </div>
+      )}
     </Wrapper>
   );
 };
